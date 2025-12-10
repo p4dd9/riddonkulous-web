844ae5e632c389f425a673abfd4dc9cc05e95a8f
Patrick Obermueller <pat.obermueller@gmail.com>
2025-12-10 19:34:12 +0100

    sd

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
 					</Link>
 					{riddleOfTheDay.date && <span> on {formatDate(riddleOfTheDay.date)}</span>}
 				</p>
-				<div className="relative py-4 px-2 rounded-lg min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh] w-full flex flex-col items-stretch overflow-hidden border-2">
+				<div className="relative py-1 px-2 rounded-lg min-h-[50vh] md:min-h-[60vh] lg:min-h-[70vh] w-full flex flex-col items-stretch overflow-hidden border-2">
 					<div
 						className="absolute inset-0 bg-position-[center_bottom] bg-no-repeat bg-cover rounded-lg"
 						style={{
@@ -30,7 +30,7 @@ export default async function Home() {
 						}}
 					/>
 
-					<div className="relative z-10 flex flex-col items-center justify-between w-full py-2 flex-1 min-h-full">
+					<div className="relative z-10 flex flex-col items-center justify-between w-full flex-1 min-h-full">
 						<div></div>
 						<div className="relative text-xl md:text-2xl w-[95%] md:w-[80%] lg:w-[70%] text-center flex-1 flex items-center justify-center py-4">
 							<p>{riddleOfTheDay.riddle}</p>
@@ -38,11 +38,11 @@ export default async function Home() {
 
 						<div className="flex items-center w-full gap-4 justify-between">
 							<div className="flex items-center justify-center gap-2">
-								<Image src="/icons/eye.png" alt="Eye" width={40} height={40} className="w-10 h-10" />{' '}
+								<Image src="/icons/eye.png" alt="Eye" width={28} height={28} className="w-7 h-7" />{' '}
 								{riddleOfTheDay.guessCount}
 							</div>
 							<div className="flex items-center justify-center gap-2">
-								<Image src="/icons/star.png" alt="Star" width={40} height={40} className="w-10 h-10" />{' '}
+								<Image src="/icons/star.png" alt="Star" width={28} height={28} className="w-7 h-7" />{' '}
 								{riddleOfTheDay.popularity}
 							</div>
 						</div>
