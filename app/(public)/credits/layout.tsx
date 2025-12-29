204373fcaad427c0854ebe23afabf672c24685d3
Patrick Obermueller <pat.obermueller@gmail.com>
2025-12-29 17:24:48 +0100

    df

--- app/(public)/credits/layout.tsx
+++ app/(public)/credits/layout.tsx
@@ -34,3 +34,4 @@ export default function CreditsLayout({ children }: { children: React.ReactNode 
 
 
 
+
--- app/(public)/faq/layout.tsx
+++ app/(public)/faq/layout.tsx
@@ -34,3 +34,4 @@ export default function FAQLayout({ children }: { children: React.ReactNode }) {
 
 
 
+
