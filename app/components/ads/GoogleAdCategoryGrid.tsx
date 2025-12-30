592a56785139c984b763f77f49c44949c3595942
Patrick Obermueller <pat.obermueller@gmail.com>
2025-12-30 19:06:51 +0100

    sdf

--- app/components/ads/GoogleAdCategoryGrid.tsx
+++ app/components/ads/GoogleAdCategoryGrid.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { GoogleAdSquare120 } from './GoogleAdSquare120'
 import { GoogleAdSquare200 } from './GoogleAdSquare200'
 import { GoogleAdSquare250 } from './GoogleAdSquare250'
 
@@ -12,6 +13,10 @@ export const GoogleAdCategoryGrid = () => {
 				}
 				
 				.ad-category-grid-200 {
+					display: none;
+				}
+				
+				.ad-category-grid-120 {
 					display: none;
 				}
 				
@@ -28,6 +33,13 @@ export const GoogleAdCategoryGrid = () => {
 						display: flex;
 					}
 				}
+				
+				/* Show 120x120 for screens >= 330px and < 485px */
+				@media (min-width: 330px) and (max-width: 484px) {
+					.ad-category-grid-120 {
+						display: flex;
+					}
+				}
 			`}</style>
 			{/* 250x250 ad for larger screens */}
 			<div className="ad-category-grid-250 items-center justify-center aspect-square w-full rounded-lg border-2 border-gray-500/50 overflow-hidden">
@@ -36,6 +48,10 @@ export const GoogleAdCategoryGrid = () => {
 			{/* 200x200 ad for medium screens */}
 			<div className="ad-category-grid-200 items-center justify-center aspect-square w-full rounded-lg border-2 border-gray-500/50 overflow-hidden">
 				<GoogleAdSquare200 />
+			</div>
+			{/* 120x120 ad for smaller screens */}
+			<div className="ad-category-grid-120 items-center justify-center aspect-square w-full rounded-lg border-2 border-gray-500/50 overflow-hidden">
+				<GoogleAdSquare120 />
 			</div>
 		</>
 	)
---
+++ app/components/ads/GoogleAdSquare120.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { GoogleAdsenseUnit } from './GoogleAdsenseUnit'
+
+export const GoogleAdSquare120 = () => {
+	return (
+		<GoogleAdsenseUnit
+			adClient="ca-pub-6902354361648358"
+			adSlot="9293300925"
+			width={120}
+			height={120}
+			fullWidthResponsive={false}
+			adFormat="auto"
+		/>
+	)
+}
+
