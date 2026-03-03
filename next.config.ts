578361c2ad98824adaffdedf6147de7b7e9fa9f1
Patrick Obermueller <pat.obermueller@gmail.com>
2026-03-03 12:27:51 +0100

    df

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,19 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+	async headers() {
+		return [
+			{
+				source: '/.well-known/assetlinks.json',
+				headers: [
+					{
+						key: 'Content-Type',
+						value: 'application/json',
+					},
+				],
+			},
+		];
+	},
+}
 
 export default nextConfig
